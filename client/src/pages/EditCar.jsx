2b820cc58a5311901a41f43c527ad9f1c0d3797a
Manya Bondada <manya.bondada@gmail.com>
2023-10-13 16:07:17 -0500

    edit car pg api css fixed

--- client/src/pages/EditCar.jsx
+++ client/src/pages/EditCar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../App.css'
+import '../css/EditCar.css'
 import {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import CustomItemAPI from '../services/customItem.js'
